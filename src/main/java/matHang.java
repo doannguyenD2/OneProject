5202ee019f2eb00f6ce849cb2adac4a4b7ec45a2
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2019-12-12 22:49:00 +0700

    int commit

---
+++ src/main/java/matHang.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dharm
+ */
+public class matHang {
+    private int maHang;
+    private String nhomHanString;
+    private Double giaDouble;
+
+    public int getMaHang() {
+        return maHang;
+    }
+
+    public void setMaHang(int maHang) {
+        this.maHang = maHang;
+    }
+
+    public String getNhomHanString() {
+        return nhomHanString;
+    }
+
+    public void setNhomHanString(String nhomHanString) {
+        this.nhomHanString = nhomHanString;
+    }
+
+    public Double getGiaDouble() {
+        return giaDouble;
+    }
+
+    public void setGiaDouble(Double giaDouble) {
+        this.giaDouble = giaDouble;
+    }
+    
+}
